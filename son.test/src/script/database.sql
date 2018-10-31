e3fb9ee2facd114925a4802dc29e71cdffe5695b
jeon <jeon@DESKTOP-ED9KM0J>
2018-10-31 17:48:04 +0900

    temp_file 테이블 신규 생성

--- son.test/src/script/database.sql
+++ son.test/src/script/database.sql
@@ -14,13 +14,13 @@ CREATE TABLE IF NOT EXISTS `board` (
   `update_dt` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`idx`),
   KEY `idx` (`idx`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `board_image` (
   `img_id` int(11) NOT NULL AUTO_INCREMENT,
   `create_dt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`img_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `board_image_detail` (
   `img_id` int(11) NOT NULL,
@@ -30,8 +30,20 @@ CREATE TABLE IF NOT EXISTS `board_image_detail` (
   `file_size` varchar(50) DEFAULT NULL,
   `file_path` varchar(100) DEFAULT NULL,
   `file_extsn` varchar(20) DEFAULT NULL,
+  `file_type` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`img_id`,`img_srno`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `temp_file` (
+  `temp_file_id` int(11) NOT NULL AUTO_INCREMENT,
+  `origin_file_name` varchar(500) NOT NULL,
+  `stored_file_name` varchar(500) NOT NULL,
+  `file_size` varchar(50) NOT NULL,
+  `file_path` varchar(100) NOT NULL,
+  `file_extsn` varchar(20) NOT NULL,
+  `file_type` varchar(100) NOT NULL,
+  PRIMARY KEY (`temp_file_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
